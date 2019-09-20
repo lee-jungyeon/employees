212fbcb856c41a7a58b6a2f88e03c585fe703d5f
GD3 <GD3@DESKTOP-TQ15C6N>
2019-09-20 18:12:54 +0900

    Employees table MVC작업 완료

---
+++ employees/src/controller/GetEmployeesListServlet.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.EmployeesDao;
+import vo.Employees;
+
+
+@WebServlet("/employees/getEmployeesList")
+public class GetEmployeesListServlet extends HttpServlet {
+	private EmployeesDao employeesDao;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int limit=10;
+		if(request.getParameter("limit")!=null) {
+			limit=Integer.parseInt(request.getParameter("limit"));
+		}
+		System.out.println("limit 확인: "+limit);
+		
+		
+		employeesDao = new EmployeesDao();
+		List<Employees>list= employeesDao.selectEmployeesListByLimit(limit);
+		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/WEB-INF/views/employeesList.jsp").forward(request,  response);
+	}
+}
--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import vo.Employees;
 
 public class EmployeesDao {
 	public int selectEmployeesCount() {
@@ -38,4 +42,49 @@ public class EmployeesDao {
 		
 		return  count;
 	}
+
+public List<Employees> selectEmployeesListByLimit(int limit){
+	System.out.println("EmployeesDao selectEmployeesListByLimit parama limit :"+limit);
+	List<Employees> list = new ArrayList<Employees>();
+	Connection conn = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+	String sql="SELECT emp_no, birth_date, first_name, last_name, gender, hire_date FROM employees LIMIT ?";
+	
+	
+	try {
+		Class.forName("org.mariadb.jdbc.Driver");
+		conn=DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees","root","java1234");
+		stmt=conn.prepareStatement(sql);
+		stmt.setInt(1,limit);
+		rs=stmt.executeQuery();
+		
+		while(rs.next()) {
+			Employees employees = new Employees();
+			 employees.setEmpNo(rs.getInt("emp_no"));
+			 employees.setBirthDate(rs.getString("birth_date" ));
+			 employees.setFirstName(rs.getString("first_name" ));
+			 employees.setLastName(rs.getString("last_name" ));
+			 employees.setGender(rs.getString("gender" ));
+			 employees.setHireDate(rs.getString("hire_date"));
+			list.add(employees);	
+		}
+		
+	}catch(Exception e) {
+		
+	}finally {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	return list;
 }
+}
+
